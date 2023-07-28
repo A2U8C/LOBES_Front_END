23a8b416b5bded4aaf86640224eef3e103e9711a
Aarya-Vakharia <avakhari@usc.edu>
2023-07-28 15:58:18 -0700

    Implmented Cohort Table, with sorting and pagination

--- lobes_front_end/src/Scenes/Cohorts/index.jsx
+++ lobes_front_end/src/Scenes/Cohorts/index.jsx
@@ -1,6 +1,7 @@
-import { Box } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Card, CardContent, Skeleton } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import CohortDetails from "./CohortDetails";
+import EnhancedTable from "../../components/EnhancedTable";
 
 const mockData = [
   {
@@ -18,16 +19,87 @@ const mockData = [
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
   },
+  {
+    name: "Cohort 4",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
+  },
+  {
+    name: "Cohort 5",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
+  },
+  {
+    name: "Cohort 6",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
+  },
+  {
+    name: "Cohort 7",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
+  },
+  {
+    name: "Cohort 8",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam quisquam cum autem. Dolorem velit cumque fugiat! Ab temporibus odit ipsa maxime possimus, minus maiores rem quam illo molestias aspernatur quibusdam.",
+  },
 ];
+
+const headCells = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Name",
+  },
+];
+
+function getCohortData() {
+  return new Promise(function (resolve, reject) {
+    // some async operation here
+    setTimeout(function () {
+      // resolve the promise with some value
+      resolve(mockData);
+    }, 3000);
+  });
+}
 
 function Cohorts() {
   const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const result = await getCohortData();
+      setData(result);
+    };
+
+    getData();
+  }, []);
+
   return (
     <Box mx={2}>
-      {data.length ? (
-        <CohortDetails name="Cohort 1" />
+      {data.length < 1 ? (
+        <Card sx={{ minHeight: 200 }}>
+          <CardContent>
+            <Box
+              height="100%"
+              display="flex"
+              justifyContent="space-between"
+              flexDirection="column"
+              gap={2}
+            >
+              <Skeleton height={60} variant="rounded" animation="wave" />
+
+              <Skeleton height={300} variant="rounded" animation="wave" />
+            </Box>
+          </CardContent>
+        </Card>
       ) : (
-        <Box onClick={() => setData(mockData)}>Table</Box>
+        <EnhancedTable
+          rows={data}
+          headCells={headCells}
+          tableTitle="List of Cohorts"
+        />
       )}
     </Box>
   );
---
+++ lobes_front_end/src/components/EnhancedTable.jsx
@@ -0,0 +1,240 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { useTheme } from "@mui/material";
+import { Divider } from "@mui/material";
+import { tokens } from "../theme";
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort, headCells } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              <Typography variant="h6" fontWeight="bold">
+                {headCell.label}
+              </Typography>
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTableToolbar(props) {
+  const { title } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        width="100%"
+      >
+        <Typography variant="h3" sx={{ flex: "1 1 100%", fontWeight: "bold" }}>
+          {title}
+        </Typography>
+        <Box
+          border="1px solid black"
+          borderRadius="10px"
+          padding="5px"
+          sx={{ flex: "1 1 100%" }}
+        >
+          Search...
+        </Box>
+      </Box>
+    </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  title: PropTypes.string,
+};
+
+export default function EnhancedTable(props) {
+  const { tableTitle, rows, headCells } = props;
+
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState();
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.name);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [order, orderBy, page, rowsPerPage]
+  );
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2, px: 2 }}>
+        <EnhancedTableToolbar title={tableTitle} />
+        <Divider
+          sx={{
+            borderBottomWidth: 2,
+            bgcolor: colors.yellowAccent[500],
+            borderColor: colors.yellowAccent[500],
+          }}
+        />
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+              headCells={headCells}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    tabIndex={-1}
+                    key={row.name}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <TableCell component="th" id={labelId} scope="row">
+                      <Typography variant="h5">{row.name}</Typography>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 15]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
